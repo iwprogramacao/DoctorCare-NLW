7e29249cbc4a171023e878b0f7b6051ded6b4a1b
Igor Wiepieski <iw.programacao@gmail.com>
2022-05-04 12:49:06 -0300

    Day 03. CSS n HTML Upgrades, new functions

--- main.js
+++ main.js
@@ -2,12 +2,29 @@ function onScroll() {
   if (scrollY === 0) {
     navigation.classList.remove('scroll')
   } else {
-    navigation.classList.add("scroll")
+    navigation.classList.add('scroll')
   }
 }
+
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
+
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+#home, 
+#home img, 
+#home .stats,
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about .content`)
